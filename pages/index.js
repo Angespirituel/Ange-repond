ac5c5a3a173385906c0362a4f3fcb089c1d8d4fd
Angespirituel <oyonotvshow@gmail.com>
2025-06-14 16:26:28 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>👼 Bienvenue sur Ange Répond</h1>
+      <p>Pose ta question divine ici... ✨</p>
+    </div>
+  );
+}
